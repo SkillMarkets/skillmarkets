d9d74b1f9e2eaa6d55a4242a9f6c040432a6fff4
Sardorbek Nematov <sardorbeknematov52@gmail.com>
2025-12-18 09:45:57 +0300

    chore: switch to PostgreSQL for Render

--- config.py
+++ config.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
+    # Render сам даёт переменную DATABASE_URL при подключении PostgreSQL
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql+pymysql://root:@localhost/skillmarkets'
+        'postgresql://localhost/skillmarkets'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
     STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
